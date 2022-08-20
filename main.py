da354877dc5fd84d80243e8b8e2b4f420369335a
yamashita-tiemi <17800160777@estudante.ufjf.br>
2022-08-19 22:22:31 -0300

    layout and server

--- main.py
+++ main.py
@@ -15,23 +15,29 @@ class MainApp(App):
         """
         Metodo que gera o aplicativo com o main widget
         """
-        self._widget = MainWidget()
+        self._widget = MainWidget(scan_time=1000, server_ip='127.0.0.1', server_port=502,
+        modbus_addrs = {
+                'estado_mot': {'addr':800, 'tipo': 'coil', 'mult': None},
+                'freq_des': {'addr': 799, 'tipo': 'hold', 'mult': 1},
+                't_part': {'addr': 798, 'tipo': 'hold', 'mult': 10},
+                'freq_mot': {'addr': 800, 'tipo': 'input', 'mult': 10},
+                'tensao': {'addr': 801, 'tipo': 'input', 'mult': 1},
+                'rotacao': {'addr': 803, 'tipo': 'input', 'mult': 1},
+                'pot_entrada': {'addr': 804, 'tipo': 'input', 'mult': 10},
+                'corrente': {'addr': 805, 'tipo': 'input', 'mult': 100},
+                'temp_estator': {'addr': 806, 'tipo': 'input', 'mult': 10},
+                'vz_entrada': {'addr': 807, 'tipo': 'input', 'mult': 100},
+                'nivel': {'addr': 808, 'tipo': 'input', 'mult': 10},
+                'nivel_h': {'addr': 809, 'tipo': 'discrete', 'mult': None},
+                'nivel_1': {'addr': 810, 'tipo': 'discrete', 'mult': None},
+                'Solenoide 1': {'addr': 801, 'tipo': 'coil', 'mult': None},
+                'Solenoide 2': {'addr': 802, 'tipo': 'coil', 'mult': None},
+                'Solenoide 3': {'addr': 803, 'tipo': 'coil', 'mult': None},
+
+            
+        },
+        )
         return self._widget
-    
-    #     self._widget = MainWidget(scan_time=1000, server_ip='127.0.0.1',server_port=502,
-    #     modbus_addrs = {
-    #         'fornalha': 1000,
-    #         'gas_ref': 1001,
-    #         'gasolina': 1002,
-    #         'nafta': 1003,
-    #         'querosene': 1004,
-    #         'diesel': 1005,
-    #         'oleo_lub': 1006,
-    #         'oleo_comb': 1007,
-    #         'residuos': 1008
-    #     }    
-    #     )
-    #     return self._widget
     # def on_stop(self):
     #     """"
     #     Metodo executado quando a alicaçao e fechada
--- mainwidget.py
+++ mainwidget.py
@@ -1,7 +1,63 @@
+from multiprocessing.sharedctypes import Value
+import random
 from kivy.uix.boxlayout import BoxLayout
+# from popups import ModbusPopup, ScanPopup, DataGraphPopup, HistGraphPopup
+from pyModbusTCP.client import ModbusClient
+from kivy.core.window import Window
+from threading import Thread
+from time import sleep
+from datetime import datetime
+# from timeseriesgraph import TimeSeriesGraph
+# from bdhandler import BDHandler
+from kivy_garden.graph import LinePlot
 
 class MainWidget(BoxLayout):
     """
     Widget principal da aplicaçao
     """
-    pass
+    _updateThread = None
+    _updateWidgets = True
+    _tags = {}
+    _max_points = 20
+
+    def __init__(self, **kwargs):
+        """
+        Construtor do widget principal
+        """
+        super().__init__()
+        self._scan_time = kwargs.get('scan_time')
+        self._serverIP = kwargs.get('server_ip')
+        self._serverPort = kwargs.get('server_port')
+        # self._modbusPopup = ModbusPopup(self._serverIP,self._serverPort)
+        # self._scanPopup = ScanPopup(scantime=self._scan_time)
+        self._modbusClient = ModbusClient(host=self._serverIP,port=self._serverPort)
+        self._funcs = {
+            'coil': self._modbusClient.read_coils,
+            'hold': self._modbusClient.read_holding_registers,
+            'input': self._modbusClient.read_input_registers,
+            'discrete': self._modbusClient.read_discrete_inputs,
+        }
+        self._meas = {}
+        self._meas['timestamp'] = None
+        self._meas['values'] = {}
+        self._tags = kwargs.get('modbus_addrs')
+        for key,value in kwargs.get('modbus_addrs').items():
+            if key == 'nivel':
+                plot_color = (0,0,1,1)
+            else:
+                plot_color = (random.random(),random.random(),random.random(),1)
+            value['color']= plot_color
+        # self._graph = DataGraphPopup(self._max_points, self._tags['fornalha']
+        # ['color'])
+        # self._hgraph = HistGraphPopup(tags=self._tags)
+        # self._db = BDHandler(kwargs.get('db_path'), self._tags)
+
+    def readData(self):
+        """
+        Metodo para a leitura dos dados por meio do protocolo MODBUS
+        """
+        self._meas['timestamp'] = datetime.now()
+        for key,value in self._tags.items():
+            self._meas['values'][key] = self._funcs[value['tipo']](value['addr'],1)[0]
+            if value['mult'] is not None:
+                self._meas['values'][key] /= value['mult']
